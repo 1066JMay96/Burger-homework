de9def02d257ff06992ec3885ad460c3b175387b
1066JMay96 <williammay.03@gmail.com>
2020-03-07 17:55:56 -0500

    controllers

---
+++ controllers/burger_controller.js
@@ -0,0 +1,57 @@
+const connection = require('../config/connection');
+
+const getBurgers = () => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM burgers', (err, burgersdata) => {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      }
+      resolve(burgersdata);
+    });
+  });
+};
+const createburger = (burgersObj) => {
+  return new Promise((resolve, reject) => {
+    connection.query('INSERT INTO burgers SET?', burgersObj, (err, burgersdata) => {
+      if (err) {
+        console.log(err);
+      return reject(err);
+      }
+      resolve(burgersdata)
+    })
+  })
+}
+const updateBurgers = (burgersObj, burgersId) => {
+  return new Promise((resolve, reject) => {
+    connection.query('UPDATE burgers SET ? WHERE id = ?', [burgersObj, burgersId], (err, burgersdata) => {
+    
+      if (err) {
+        console.log(err);
+        return reject(err);
+    } 
+    else if (burgersData.affectedRows === 0){
+   return resolve({message: "Couldn't find that burger item!"});
+   }
+      resolve({ message: 'burgers menu updated successfully!' })
+    })
+  })
+}
+
+
+const deleteBurgers = (burgersId) => {
+ return new Promise((resolve, reject) => {
+  connection.query('DELETE FROM burgers WHERE id = ?', [burgersId],(err, burgerdata) => {
+    if (err) {
+      console.log(err);
+      return reject(err);
+    } else if (CharacterData.affectedRows === 0) {
+      return resolve({ message: "couldn't find that burger id"});
+    }
+
+    resolve({ message:'Burger Devoured!'})
+  });
+  });
+};
+
+module.exports = {getBurgers, createburger, updateBurgers, deleteBurgers };
