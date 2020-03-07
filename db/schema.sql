7464294d9bc1a9b705869d34f9006e566146bf02
1066JMay96 <williammay.03@gmail.com>
2020-03-06 21:11:03 -0500

    commmitted schema

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS burger_db;
+
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(55) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO burgers
+
+VALUES
+('CheeseBurger'),
+('VeggieBurger'),
+('TripleDoubleBurger'),
+('HeartAttackBurger'),
+('DiabetisBurger');
+
+SELECT * FROM burgers;
+
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const connection = require('./config/connection');
+
+const routes = require('./routes');
+
+app.use(express.json());
+app.use(express.urlencoded({exptended: true}));
+app.use(express.static('public'));
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+connection.connection(err => {
+  if (err){
+    throw new Error(err);
+  }
+  app.listen(PORT, () =>console.log(`Now listening on port ${PORT}`));
+});
